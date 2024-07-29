b1a6904f3092fc77da5b78486bfa3d1a46e60326
Mariana Cardoso <mariana.cbarros16@gmail.com>
2024-07-29 14:22:51 -0300

    fix: business rule error

--- app/Http/Controllers/CompanyFinancialsController.php
+++ app/Http/Controllers/CompanyFinancialsController.php
@@ -23,7 +23,10 @@ class CompanyFinancialsController extends Controller
     {
         $financials = CompanyFinancials::findOrFail($id);
 
-        if ($request->due_date < date('Y-m-d') && (float) $request->fees != (float) $financials->fees) {
+        if (($request->value != $financials->value && $request->due_date < date('Y-m-d')) || 
+            ($request->due_date < date('Y-m-d') && (float) $request->fees != (float) $financials->fees)) 
+        {
+            dd('caiu aqui 1');
             $newValue = (float) $request->value * (1 + ((float) $request->fees / 100));
         }
 
